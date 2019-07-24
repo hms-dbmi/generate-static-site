ec949b3d54d734623d1014916f0bf0996738333c
Chuck McCallum <chuck_mccallum@hms.harvard.edu>
2019-07-24 15:58:24 -0400

    checkpoint

---
+++ generate.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+set -o errexit
+
+die() { set +v; echo "$*" 1>&2 ; exit 1; }
+
+[ "$#" = 1 ] || die 'Expects one argument, s3 bucket name (= site hostname).'
+
+export HOST="$1"
+[[ "$HOST" = *.org ]] || [[ "$HOST" = *.io ]] || die "'$HOST' does not look like a hostname."
+
+CLEAN=`echo "$HOST" | sed -e 's/[^[:alnum:]]/-/g'`
+
+DIR=`dirname $0`
+POLICY=`erb "$DIR/aws-template/policy.json.erb"`
+echo "Policy doc: $POLICY"
+
+POLICY_NAME="$CLEAN-policy"
+POLICY_ARN=`aws iam create-policy --policy-name "$POLICY_NAME" --policy-document "$POLICY"  --description "Auto-generated policy for $HOST." --query 'Policy.Arn'`
+echo "Created $POLICY_NAME with ARN $POLICY_ARN"
+
+USER_NAME="$CLEAN-user"
+aws iam create-user \
+  --user-name "$USER_NAME"
+echo "Created $USER_NAME"
+
+aws iam attach-user-policy \
+  --user-name "$USER_NAME" \
+  --policy-arn "$POLICY_ARN"
+echo  "Attached policy"
