acfa82d997d83dc8b3f78305cd4fd34f50223cf6
Chuck McCallum <chuck_mccallum@hms.harvard.edu>
2019-07-25 11:48:37 -0400

    formatting

--- generate.sh
+++ generate.sh
@@ -2,6 +2,8 @@
 set -o errexit
 
 die() { set +v; echo "$*" 1>&2 ; exit 1; }
+b=$(tput bold)
+n=$(tput sgr0)
 
 [ "$#" = 1 ] || die 'Expects one argument, s3 bucket name (= site hostname).'
 
@@ -12,18 +14,23 @@ CLEAN=`echo "$HOST" | sed -e 's/[^[:alnum:]]/-/g'`
 
 DIR=`dirname $0`
 POLICY=`erb "$DIR/aws-template/policy.json.erb"`
-echo "Policy doc: $POLICY"
+echo "${b}Filled policy template${n} $POLICY"
 
 POLICY_NAME="$CLEAN-policy"
-POLICY_ARN=`aws iam create-policy --policy-name "$POLICY_NAME" --policy-document "$POLICY"  --description "Auto-generated policy for $HOST." --query 'Policy.Arn' --output text`
-echo "Created $POLICY_NAME with ARN $POLICY_ARN"
+POLICY_ARN=`aws iam create-policy \
+              --policy-name "$POLICY_NAME" \
+              --policy-document "$POLICY" \
+              --description "Auto-generated policy for $HOST." \
+              --query 'Policy.Arn' \
+              --output text`
+echo "${b}Created policy${n} $POLICY_NAME ${b}with ARN${n} $POLICY_ARN"
 
 USER_NAME="$CLEAN-user"
 aws iam create-user \
-  --user-name "$USER_NAME"
-echo "Created $USER_NAME"
+  --user-name "$USER_NAME" > /dev/null
+echo "${b}Created user${n} $USER_NAME"
 
 aws iam attach-user-policy \
   --user-name "$USER_NAME" \
   --policy-arn "$POLICY_ARN"
-echo  "Attached policy"
+echo  "${b}Attached policy${n}"
