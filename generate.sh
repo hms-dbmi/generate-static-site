ef5add4c2d29f7724ed6fccdb457ffb5e28cc780
Chuck McCallum <chuck_mccallum@hms.harvard.edu>
2019-07-25 12:07:35 -0400

    Secret keys

--- generate.sh
+++ generate.sh
@@ -18,11 +18,11 @@ echo "${b}Filled policy template${n} $POLICY"
 
 POLICY_NAME="$CLEAN-policy"
 POLICY_ARN=`aws iam create-policy \
-              --policy-name "$POLICY_NAME" \
-              --policy-document "$POLICY" \
-              --description "Auto-generated policy for $HOST." \
-              --query 'Policy.Arn' \
-              --output text`
+  --policy-name "$POLICY_NAME" \
+  --policy-document "$POLICY" \
+  --description "Auto-generated policy for $HOST." \
+  --query 'Policy.Arn' \
+  --output text`
 echo "${b}Created policy${n} $POLICY_NAME ${b}with ARN${n} $POLICY_ARN"
 
 USER_NAME="$CLEAN-user"
@@ -33,4 +33,17 @@ echo "${b}Created user${n} $USER_NAME"
 aws iam attach-user-policy \
   --user-name "$USER_NAME" \
   --policy-arn "$POLICY_ARN"
-echo  "${b}Attached policy${n}"
+echo "${b}Attached policy${n}"
+
+SECRET_ACCESS_KEY=`aws iam create-access-key \
+  --user-name "$USER_NAME" \
+  --query 'AccessKey.SecretAccessKey' \
+  --output text`
+echo "${b}Secret access key${n} $SECRET_ACCESS_KEY"
+
+# Alternatively, we could get both the secret key and the ID at creation time.
+ACCESS_KEY_ID=`aws iam list-access-keys \
+  --user-name "$USER_NAME" \
+  --query 'AccessKeyMetadata[0].AccessKeyId' \
+  --output text`
+echo "${b}Access key ID${n} $ACCESS_KEY_ID"
